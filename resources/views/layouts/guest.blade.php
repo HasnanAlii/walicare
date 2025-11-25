6d8bd73558978dd04826edb760a2d7a295cb3c80
HasnanAlii <ahasnan32@gmail.com>
2025-11-25 15:36:08 +0700

    revisi

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,7 +10,8 @@
 
 
     <link rel="preconnect" href="https://fonts.bunny.net">
-    
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
